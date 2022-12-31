f3e6cccf08648de389517f16e41fb7826a0a7c90
goku$9914 <anshumaantiwari123@gmail.com>
2022-12-31 15:42:59 +0530

    commit2 editing app.js

--- app.js
+++ app.js
@@ -145,6 +145,6 @@ app.get("/:customListName",function(req,res){
 
 });
 
-app.listen("3000",function(){
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is running on port 3000");
 });
